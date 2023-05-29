90b299a555b96559b15fcbefc9c4517c9e6c0f4e
SARAIFIGUEROA <101596430+SARAIFIGUEROA@users.noreply.github.com>
2023-05-29 17:05:29 -0300

    Add files via upload

---
+++ Hamburguesasfijo/dom.js
@@ -0,0 +1,10 @@
+var total=0
+function sumador(valor,tipo){
+    var detalle=document.getElementById("detalle")
+    var elemento=document.createElement("p")
+    elemento.innerHTML=tipo+"$"+valor;
+    detalle.appendChild(elemento)
+    total=total+valor
+
+    var=
+}
